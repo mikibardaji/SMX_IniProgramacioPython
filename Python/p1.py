e8d87c09748b3708b7f055aec0ec7e27b5ae990b
Miquel Angel Bardaji <mikibardaji@hotmail.com>
2026-01-08 20:40:31 +0100

    Add files via upload

---
+++ Python/p1.py
@@ -0,0 +1,2 @@
+# Primer programa en java
+print("Hello World")
---
+++ Python/p3.py
@@ -0,0 +1,12 @@
+# demanar valors al usuari
+print("posa el teu nom")
+#valors la instruccio es input
+nom = input()
+print("posa la teva adreça: ")
+#valors la instruccio es input
+direccio = input()
+print("posa que estas estudiant: ")
+#valors la instruccio es input
+estudis = input()
+print("Et dius ", nom, 
+'vius a ', direccio, ' i estudies', estudis)
