384f6490cdb1f77da85bde94dde7f10b7072a567
Releeu <109560876+GhostBabe448@users.noreply.github.com>
2026-01-08 21:08:48 +0100

    Add files via upload

---
+++ 20260108/Carlos/SumaRestaMultiDiv.py
@@ -0,0 +1,15 @@
+Numero1= int(input("Primer valor: "))
+Numero2 = int(input("Segon valor: "))
+Numero3 = int(input("Tercer valor: "))
+
+suma = Numero1 + Numero2 + Numero3
+print("Suma:", suma)
+
+resta = Numero1 - Numero2 - Numero3
+print("Resta:", resta)
+
+multi = Numero1 * Numero2 * Numero3
+print("Multiplicació:", multi)
+
+div = Numero1 / Numero2
+print("Divisió:", div)
