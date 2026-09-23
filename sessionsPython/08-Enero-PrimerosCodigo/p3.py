c881858ffb3367f535789bf93a2adcdbbd90c05e
Miquel Angel Bardaji <mikibardaji@hotmail.com>
2026-09-23 11:12:51 +0200

    Delete sessionsPython directory

--- sessionsPython/08-Enero-PrimerosCodigo/p3.py
+++
@@ -1,7 +0,0 @@
-# Fes un programa que demana el teu 
-# nom i cognom en un sol cop i 
-# despres els mostra, 
-# tot amb majuscula i després tot amb minuscula. https://www.w3schools.com/python/python_strings_modify.asp
-nom_cognom = input("Pon tu nombre y apellido: ")
-print("Mayuscula " , nom_cognom.upper() )
-print("Minuscula " , nom_cognom.lower() )
--- sessionsPython/15-Enero-bucles/ex6.py
+++
@@ -1,8 +0,0 @@
-numero = 5
-cont_num=0
-while (numero!=666):
-    numero = int(input("pon un numero "))
-    if (numero != 666):
-        print("has escrito  ", numero)
-        cont_num +=1
-print("Has introducido ", cont_num , "")
